c8347f827b0b5170f59c799e0768155da394af1b
Jamie Parsons <jhelenparsons@gmail.com>
2017-05-19 11:54:11 -0600

    gems and variables

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'pry'
+gem 'babbler'
+
+puts Babbler.babble
---
+++ gems_and_classes.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'babbler'
+
+puts Babbler.babble
+
+first_name = 'jamie'
+last_name = 'parsons'
+binding.pry
+age = 36
